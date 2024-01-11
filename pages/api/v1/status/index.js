e40ef7fd0d528fde133edc1c3ce9626ffac5d019
Matheus Cordeiro <matheuscordeirop@gmail.com>
2024-01-11 17:49:37 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from 'infra/database.js';
 
 const status = async (request, response) => {
-  const result = await database.query('SELECT 1+1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ response: 'hello World' });
+  const dbName = process.env.POSTGRES_DB;
+  const dbMaxConnections = await database.query('SHOW max_connections;');
+  const dbOpenedConnections = await database.query({
+    text: `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1 AND state = 'active';`,
+    values: [dbName],
+  });
+  const dbVersion = await database.query('SHOW server_version;');
+
+  const databaseResponse = {
+    max_connections: Number.parseInt(dbMaxConnections.rows[0].max_connections),
+    opened_connections: dbOpenedConnections.rows[0].opened_connections,
+    version: dbVersion.rows[0].server_version,
+  };
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: databaseResponse,
+    },
+  });
 };
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,22 @@
-test('GET to /api/v1/status should return 200', async () => {
+test('GET to /api/v1/status should return 200 and correct response format', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
-
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const { max_connections, opened_connections, version } =
+    responseBody.dependencies.database;
+
+  expect(typeof max_connections).toBe('number');
+  expect(max_connections).toBeGreaterThan(0);
+
+  expect(typeof opened_connections).toBe('number');
+  expect(opened_connections).toBe(1);
+
+  expect(typeof version).toBe('string');
+  expect(version.length).toBeGreaterThan(0);
 });
